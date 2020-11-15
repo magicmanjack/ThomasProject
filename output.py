561a7cda4cae709dae43863b33d1bece15298ff2
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2020-11-15 22:36:11 +1300

    YEs

--- output.py
+++ output.py
@@ -1,5 +1,6 @@
-z = x + y
-main()
+def test(a, b, c):
+    a = 'a'
+    b = 'b'
 x = 5
 y = 6
 z = x + y
--- transpiler.py
+++ transpiler.py
@@ -7,6 +7,8 @@ c_datatypes = {"int ","float ","double ","bool ","short ","long ","char "}
 with open(c_file_str, "r") as f:
     lines = f.readlines()
     for l in lines:
+        if "main(" in l:
+            indent -= 1
         if "{" in l:
             indent += 1
         if "}" in l:
@@ -14,7 +16,17 @@ with open(c_file_str, "r") as f:
 
         for datatype in c_datatypes:
             if datatype in l:
-                with open(python_file_str, "a+") as output_f:
-                    output_f.write(l[l.find(datatype)+ len(datatype):].replace(";", ""))
-                
-        print(l)
+                l_out = l.replace(datatype, "").replace(";", "").strip() + "\n"
+            
+                if "(" in l and not "main(" in l:
+                    l_out = "def {}:\n".format(l_out.strip())
+                else:
+                    l_out = "{indents}{statement}".format(indents="    " * indent, statement=l_out)
+
+                if not "main(" in l:
+                    with open(python_file_str,"a+") as f1:
+                        f1.write(l_out)
+                    
+                print(l)
+
+        

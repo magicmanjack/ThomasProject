4969f917c324e90d4e0405877fe72485060aee8c
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2020-11-16 13:20:52 +1300

    Yay.py

--- output.py
+++
@@ -1,6 +0,0 @@
-def test(a, b, c):
-    a = 'a'
-    b = 'b'
-x = 5
-y = 6
-z = x + y
--- transpiler.py
+++ transpiler.py
@@ -4,6 +4,11 @@ python_file_str = input("Enter target python file: ");
 indent = 0
 
 c_datatypes = {"int ","float ","double ","bool ","short ","long ","char "}
+
+analagous_func = {
+    "printf(" : "print("
+}
+
 with open(c_file_str, "r") as f:
     lines = f.readlines()
     for l in lines:
@@ -13,12 +18,12 @@ with open(c_file_str, "r") as f:
             indent += 1
         if "}" in l:
             indent -= 1
-
+        l_out = ""
         for datatype in c_datatypes:
             if datatype in l:
                 l_out = l.replace(datatype, "").replace(";", "").strip() + "\n"
             
-                if "(" in l and not "main(" in l:
+                if "(" in l and not "main(" in l and not "=" in l:
                     l_out = "def {}:\n".format(l_out.strip())
                 else:
                     l_out = "{indents}{statement}".format(indents="    " * indent, statement=l_out)
@@ -26,7 +31,21 @@ with open(c_file_str, "r") as f:
                 if not "main(" in l:
                     with open(python_file_str,"a+") as f1:
                         f1.write(l_out)
+                        
+        for func in analagous_func:
+            if func in l:
+                #converts function to python function
+                l_out = l.replace(func, analagous_func[func]).replace(";", "").strip() + "\n"
+                first_slice = l_out[:l_out.find("\"", l_out.find("\"") + 1) + 1]
+                second_slice = l_out[l_out.find("\"", l_out.find("\"") + 1) + 1:]
+                if "," in second_slice:
+                  second_slice = second_slice.replace(",", "%", 1).strip()
+                  second_slice = "{}({}){}".format(second_slice[0], second_slice[1:second_slice.find(")")], second_slice[second_slice.find(")"):])       
+                  l_out = first_slice + second_slice
+
                     
-                print(l)
+                with open(python_file_str,"a+") as f1:
+                        f1.write(l_out)
 
         
+
